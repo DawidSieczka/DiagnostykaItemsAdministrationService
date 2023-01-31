f484eb07946f1943c595047adbae47e5b6fabe99
DawidSieczka <dawid.sieczka.praca@gmail.com>
2023-01-31 12:39:16 +0100

    added db config

--- DiagnostykaItemsAdministrationService/DiagnostykaItemsAdministrationService.Api/Program.cs
+++ DiagnostykaItemsAdministrationService/DiagnostykaItemsAdministrationService.Api/Program.cs
@@ -1,11 +1,21 @@
 using DiagnostykaItemsAdministrationService.Application.Operations.Items.Queries.GetItem;
+using DiagnostykaItemsAdministrationService.Persistence;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddMediatR(typeof(GetItemQuery));
+
+builder.Services.AddDbContext<AppDbContext>(dbBuilder =>
+    dbBuilder
+        .UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), options => options.CommandTimeout(120)
+            .EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)));
+
+using var appDbContext = builder.Services.BuildServiceProvider().GetRequiredService<AppDbContext>();
+appDbContext.Database.Migrate();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- DiagnostykaItemsAdministrationService/DiagnostykaItemsAdministrationService.Persistence/AppDbContext.cs
+++ DiagnostykaItemsAdministrationService/DiagnostykaItemsAdministrationService.Persistence/AppDbContext.cs
@@ -10,4 +10,5 @@ public class AppDbContext : DbContext
 	{
 		base.OnModelCreating(modelBuilder);
 	}
+
 }
